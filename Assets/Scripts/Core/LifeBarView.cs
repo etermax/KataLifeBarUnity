3e91e74f1a82c850cb4dc00435f6831b23714143
guidoPaglie <guido.pagliettini@gmail.com>
2020-07-17 20:55:27 -0300

    First iteration View

---
+++ Assets/Scripts/Core/LifeBarView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Core;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBarView : MonoBehaviour
+{
+    [SerializeField] private Button _attackButton;
+    [SerializeField] private TMP_InputField _attackValue;
+    [SerializeField] private Button _healButton;
+    [SerializeField] private TMP_InputField _healValue;
+
+    [SerializeField] private Slider _lifeBar;
+    
+    private Life _life;
+
+    private void Awake()
+    {
+        _life = new Life();
+        _lifeBar.value = _life.CurrentValue;
+    }
+    
+    private void Start()
+    {
+        _attackButton.onClick.AddListener(TakeDamage);
+        _healButton.onClick.AddListener(TakeHeal);
+    }
+
+    private void TakeHeal()
+    {
+        var healValue = _healValue.text;
+        _life.ReceiveHeal(Int32.Parse(healValue));
+        _lifeBar.value = _life.CurrentValue;
+    }
+    
+    private void TakeDamage()
+    {
+        var damageValue = _attackValue.text;
+        _life.ReceiveDamage(Int32.Parse(damageValue));
+        _lifeBar.value = _life.CurrentValue;
+    }
+}
